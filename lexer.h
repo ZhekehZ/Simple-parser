115b5c229218e29c59d88bc12b7342b5aa51a786
Eugene Kravchenko <zzzheka97@gmail.com>
2021-04-07 16:14:27 +0300

    finish lexer

--- lexer.h
+++ lexer.h
@@ -1,105 +1,348 @@
 #pragma once
 #include <array>
 #include <vector>
-#include <optional>
+#include <variant>
 
 namespace lexer {
 
+    enum class kind {
+        WHITESPACE, // \t\r\n...
+        IDENTIFIER, // [a-zA-Z]+
+        CONSTANT,   // [0-9]+
+        ASSIGNMENT, // =
+        OPEN,       // (
+        CLOSE,      // )
+        IF,         // if
+        WHILE,      // while
+        END,        // end
+        OPERATOR,   // <>+-*/
+    };
+
     struct token {
         uint32_t begin, len;
+        kind type;
     };
 
-    template <typename T>
-    concept Lexer = requires (std::vector<token> & vector, uint32_t uint, std::string_view string_view) {
-        { T::parse(vector, uint, string_view) } -> std::same_as<std::optional<uint32_t>>;
+    struct error {
+        char const * cause;
+        uint32_t pos;
     };
 
-    template<uint32_t LEN, char const (&TOK)[LEN]>
-    struct token_match {
-        static std::optional<uint32_t> parse(std::vector<token> & output, uint32_t pos, std::string_view str) {
-            if (str.empty()) {
-                return std::nullopt;
-            }
+    using lexer_result = std::variant<uint32_t, error>;
 
-            for (auto len = 0u; len < LEN; ++len) {
-                if (str[pos + len] != TOK[len]) {
-                    return std::nullopt;
+    bool constexpr is_success(lexer_result const & l) {
+        return std::holds_alternative<uint32_t>(l);
+    }
+
+    template<typename T>
+    concept Lexer = requires(std::vector<token> &vector, uint32_t uint, std::string_view string_view) {
+        { T::parse(vector, uint, string_view) } -> std::same_as<lexer_result>;
+    };
+
+    namespace errors {
+    #define CREATE_ERROR(message) constexpr char message[] = #message
+
+        CREATE_ERROR(STRING_IS_TOO_SHORT);
+        CREATE_ERROR(INVALID_SYMBOL);
+        CREATE_ERROR(IDENTIFIER_OR_CONSTANT_EXPECTED);
+        CREATE_ERROR(UNCLOSED_PARENTHESIS);
+        CREATE_ERROR(UNFINISHED_STATEMENT);
+
+    #undef CREATE_ERROR
+    }
+
+    namespace combinators {
+
+        template<uint32_t LEN, char const (&TOK)[LEN + 1], kind KIND>
+        requires (LEN > 0)
+        struct token_match {
+            static lexer_result parse(std::vector<token> &output, uint32_t pos, std::string_view str) {
+                if (str.size() < pos + LEN) {
+                    return error {
+                        .cause = errors::STRING_IS_TOO_SHORT,
+                        .pos = static_cast<uint32_t>(str.size()),
+                    };
                 }
-            }
 
-            output.template emplace_back(pos, LEN);
-            return {pos + LEN};
-        }
-    };
-
-    template<uint32_t LEN, char const (&CHARS)[LEN]>
-    struct any_char {
-        static std::optional<uint32_t> parse(std::vector<token> & output, uint32_t pos, std::string_view str) {
-            if (str.empty()) {
-                return std::nullopt;
-            }
-
-            for (auto idx = 0u; idx < LEN; ++idx) {
-                if (str[pos + idx] == CHARS[idx]) {
-                    output.template emplace_back(pos, 1);
-                    return {pos + 1};
+                for (auto len = 0u; len < LEN; ++len) {
+                    if (str[pos + len] != TOK[len]) {
+                        return error {
+                            .cause = errors::INVALID_SYMBOL,
+                            .pos = pos + len,
+                        };
+                    }
                 }
+
+                output.emplace_back(pos, LEN, KIND);
+                return pos + LEN;
             }
+        };
 
-            return std::nullopt;
-        }
-    };
+        template<uint32_t LEN, char const (&CHARS)[LEN + 1], kind KIND>
+        requires (LEN > 0)
+        struct any_char {
+            static lexer_result parse(std::vector<token> &output, uint32_t pos, std::string_view str) {
+                if (str.size() < pos + 1) {
+                    return error {
+                        .cause = errors::STRING_IS_TOO_SHORT,
+                        .pos = static_cast<uint32_t>(str.size()),
+                    };
+                }
 
-    template<int (*GOOD_CHAR)(int)>
-    struct symbols {
-        static std::optional<uint32_t> parse(std::vector<token> & output, uint32_t pos, std::string_view str) {
-            if (str.empty() || !GOOD_CHAR(str[pos])) {
-                return std::nullopt;
+                for (auto idx = 0u; idx < LEN; ++idx) {
+                    if (str[pos] == CHARS[idx]) {
+                        output.emplace_back(pos, 1, KIND);
+                        return pos + 1;
+                    }
+                }
+
+                return error {
+                    .cause = errors::INVALID_SYMBOL,
+                    .pos = pos,
+                };
             }
+        };
 
-            auto curr_pos = pos;
-            while (curr_pos < str.size() && GOOD_CHAR(str[curr_pos])) {
-                ++curr_pos;
+        template<int (*GOOD_CHAR)(int), kind KIND>
+        requires (bool(GOOD_CHAR))
+        struct symbols {
+            static lexer_result parse(std::vector<token> &output, uint32_t pos, std::string_view str) {
+                if (str.size() <= pos) {
+                    return error {
+                        .cause = errors::STRING_IS_TOO_SHORT,
+                        .pos = static_cast<uint32_t>(str.size()),
+                    };
+                }
+
+                if (!GOOD_CHAR(str[pos])) {
+                    return error{
+                        .cause = errors::INVALID_SYMBOL,
+                        .pos = pos
+                    };
+                }
+
+                auto curr_pos = pos;
+                while (curr_pos < str.size() && GOOD_CHAR(str[curr_pos])) {
+                    ++curr_pos;
+                }
+
+                output.emplace_back(pos, curr_pos - pos, KIND);
+                return curr_pos;
             }
+        };
 
-            output.template emplace_back(curr_pos, curr_pos - pos);
-            return {curr_pos};
-        }
-    };
+        template<char SYM, kind KIND>
+        struct symbol {
+            static lexer_result parse(std::vector<token> &output, uint32_t pos, std::string_view str) {
+                if (str.size() <= pos) {
+                    return error {
+                        .cause = errors::STRING_IS_TOO_SHORT,
+                        .pos = static_cast<uint32_t>(str.size()),
+                    };
+                }
 
-    template <Lexer ... LEXERS>
-    struct sequence {
-        static std::optional<uint32_t> parse(std::vector<token> & output, uint32_t pos, std::string_view str) {
-            std::optional<uint32_t> status(pos);
-            (void) (
-                ( status = LEXERS::parse(output, status.value(), str)
-                , status.has_value()
-                ) && ...
-            );
-            return status;
-        }
-    };
+                if (SYM != str[pos]) {
+                    return error{
+                        .cause = errors::INVALID_SYMBOL,
+                        .pos = pos
+                    };
+                }
+
+                output.emplace_back(pos, 1, KIND);
+                return pos + 1;
+            }
+        };
+
+        template<Lexer ... LEXERS>
+        requires (sizeof ... (LEXERS) > 0)
+        struct sequence {
+            static lexer_result parse(std::vector<token> &output, uint32_t pos, std::string_view str) {
+                lexer_result status(pos);
+                (void) (
+                    (status = LEXERS::parse(output, std::get<uint32_t>(status), str)
+                    , is_success(status)
+                    ) && ...
+                );
+                return status;
+            }
+        };
+
+        template<Lexer ... LEXERS>
+        requires (sizeof ... (LEXERS) > 0)
+        struct alternative {
+            static lexer_result parse(std::vector<token> &output, uint32_t pos, std::string_view str) {
+                lexer_result status;
+                auto initial_size = output.size();
+                (void) (
+                    (output.erase(output.begin() + initial_size, output.end()) // rollback changes
+                    , status = LEXERS::parse(output, pos, str)
+                    , is_success(status)
+                    ) || ...
+                );
+                return status;
+            }
+        };
+
+        template<Lexer LEXER, bool AT_LEAST_ONE = true, bool MERGE_TOKENS = false>
+        struct many {
+            static lexer_result parse(std::vector<token> &output, uint32_t pos, std::string_view str) {
+                lexer_result status = LEXER::parse(output, pos, str);
+
+                if (!is_success(status)) {
+                    if constexpr (AT_LEAST_ONE) {
+                        return status;
+                    } else {
+                        status = pos;
+                    }
+                }
+
+                lexer_result new_status = status;
+                while (is_success(new_status)) {
+                    status = new_status;
+                    new_status = LEXER::parse(output, std::get<uint32_t>(status), str);
+
+                    if constexpr (MERGE_TOKENS) {
+                        if (is_success(new_status)) {
+                            auto last_token = output.back().len;
+                            output.pop_back();
+                            output.back().len += last_token;
+                        }
+                    }
+                }
+                return status;
+            }
+        };
+
+    }
 
     namespace token_strings {
         constexpr char IF[] = "if";
         constexpr char WHILE[] = "while";
         constexpr char END[] = "end";
-        constexpr char WHITE[] = " \t\r\n";
+        constexpr char WHITESPACE[] = " \t\r\n";
+        constexpr char OPERATOR[] = "><+-*/";
     }
 
-#define MAKE_TOKEN_LEX(str) token_match<sizeof(str), str>
-#define MAKE_ANYCHAR_LEX(str) any_char<sizeof(str), str>
+#define MAKE_TOKEN_LEX(str, kind) combinators::token_match<sizeof(str) - 1, str, kind>
+#define MAKE_ANYCHAR_LEX(str, kind) combinators::any_char<sizeof(str) - 1, str, kind>
 
-    using identifier = symbols<std::isalpha>;
-    using constant = symbols<std::isdigit>;
-    using whitespace = MAKE_ANYCHAR_LEX(token_strings::WHITE);
-    using k_if = MAKE_TOKEN_LEX(token_strings::IF);
-    using k_while = MAKE_TOKEN_LEX(token_strings::WHILE);
-    using k_end = MAKE_TOKEN_LEX(token_strings::END);
-
-
+    using identifier = combinators::symbols<std::isalpha, kind::IDENTIFIER>;
+    using constant = combinators::symbols<std::isdigit, kind::CONSTANT>;
+    using whitespaces = combinators::many<MAKE_ANYCHAR_LEX(token_strings::WHITESPACE, kind::WHITESPACE), false, true>;
+    using k_if = MAKE_TOKEN_LEX(token_strings::IF, kind::IF);
+    using k_while = MAKE_TOKEN_LEX(token_strings::WHILE, kind::WHILE);
+    using k_end = MAKE_TOKEN_LEX(token_strings::END, kind::END);
+    using operator_sym = MAKE_ANYCHAR_LEX(token_strings::OPERATOR, kind::OPERATOR);
 
 #undef MAKE_ANYCHAR_LEX
 #undef MAKE_TOKEN_LEX
 
+    template <Lexer LEX>
+    void maybe(std::vector<token> & output, uint32_t & pos, std::string_view str) {
+        auto tok = LEX::parse(output, pos, str);
+        if (is_success(tok)) {
+            pos = std::get<uint32_t>(tok);
+        }
+    }
+
+    struct expression {
+        static lexer_result parse(std::vector<token> & output, uint32_t pos, std::string_view str) {
+            using namespace combinators;
+
+            auto opened = 0u;
+            lexer_result tok;
+
+            while (true) {
+                if (is_success(tok = symbol<'(', kind::OPEN>::parse(output, pos, str))) {
+                    pos = std::get<uint32_t>(tok);
+                    ++opened;
+                }
+
+                maybe<whitespaces>(output, pos, str);
+
+                if (is_success(tok = alternative<identifier, constant>::parse(output, pos, str))) {
+                    pos = std::get<uint32_t>(tok);
+                } else {
+                    return error {
+                        .cause = errors::IDENTIFIER_OR_CONSTANT_EXPECTED,
+                        .pos = pos,
+                    };
+                }
+
+                maybe<whitespaces>(output, pos, str);
+
+                if (is_success(tok = symbol<')', kind::CLOSE>::parse(output, pos, str))) {
+                    pos = std::get<uint32_t>(tok);
+                    --opened;
+
+                    maybe<whitespaces>(output, pos, str);
+                }
+
+                if (is_success(tok = operator_sym::parse(output, pos, str))) {
+                    pos = std::get<uint32_t>(tok);
+                    maybe<whitespaces>(output, pos, str);
+                    continue;
+                }
+
+                return opened
+                        ? error {
+                            .cause = errors::UNCLOSED_PARENTHESIS,
+                            .pos = pos,
+                            }
+                        : lexer_result(pos);
+            }
+        }
+    };
+
+    using assignment = combinators::sequence<
+            identifier, whitespaces, combinators::symbol<'=', kind::ASSIGNMENT>, whitespaces, expression>; // id = expr
+
+    struct statement {
+        static lexer_result parse(std::vector<token> & output, uint32_t pos, std::string_view str) {
+            using namespace combinators;
+
+            auto opened = 0u;
+            lexer_result tok;
+
+            while (true) {
+                if (is_success(tok = alternative<k_while, k_if>::parse(output, pos, str))) {
+                    pos = std::get<uint32_t>(tok);
+                    maybe<whitespaces>(output, pos, str);
+
+                    if (!is_success(tok = expression::parse(output, pos, str))) {
+                        return tok;
+                    }
+                    pos = std::get<uint32_t>(tok);
+
+                    maybe<whitespaces>(output, pos, str);
+                    ++opened;
+                }
+
+                if (is_success(tok = assignment::parse(output, pos, str))) {
+                    pos = std::get<uint32_t>(tok);
+
+                    maybe<whitespaces>(output, pos, str);
+
+                    while (opened && is_success(tok = k_end::parse(output, pos, str))) {
+                        pos = std::get<uint32_t>(tok);
+                        maybe<whitespaces>(output, pos, str);
+                        --opened;
+                    }
+
+                    if (opened) {
+                        continue;
+                    }
+
+                    return {pos};
+                }
+
+                return error {
+                    .cause = errors::UNFINISHED_STATEMENT,
+                    .pos = pos,
+                };
+            }
+        }
+    };
+
+    using program = combinators::many<statement>;
 }
--- parser.h
+++ parser.h
@@ -1,19 +1,202 @@
 #pragma once
 
 #include <vector>
+#include <cassert>
 #include "lexer.h"
 
-template <lexer::Lexer T>
-struct many1 {
-    auto parse(uint32_t start_pos, std::string_view str) {
-        std::vector<lexer::token> results;
 
-//        auto pos = start_pos;
-//        while (true) {
-//            lexer::token result = T::parse(pos, str);
-////            if (!result.) {
-////                return result;
-////            }
-//        }
+namespace parser {
+
+    namespace ast {
+
+        enum class kind {
+            IF,
+            WHILE,
+            ASSIGNMENT,
+            VAR,
+            CONST,
+            BINOP,
+            STATEMENTS,
+        };
+
+        auto to_string(kind l) {
+            switch (l) {
+                case kind::IF: return "IF";
+                case kind::WHILE: return "WHILE";
+                case kind::ASSIGNMENT: return "ASSIGNMENT";
+                case kind::VAR: return "VAR";
+                case kind::CONST: return "CONST";
+                case kind::BINOP: return "BINOP";
+                case kind::STATEMENTS: return "STATEMENTS";
+            }
+            return "";
+        }
+
+        bool constexpr is_statement(kind k) {
+            return k == kind::ASSIGNMENT || k == kind::IF || k == kind::WHILE;
+        }
+
+        bool constexpr is_scope(kind k) {
+            return k == kind::IF || k == kind::WHILE;
+        }
+
+
+        bool constexpr is_expression(kind k) {
+            return k == kind::BINOP || k == kind::VAR || k == kind::CONST;
+        }
+
+
+        struct tree {
+            uint32_t root;
+
+            struct node {
+                static constexpr uint32_t npos = -1;
+
+                kind type;
+                uint32_t op1 = npos, op2 = npos, par = npos;
+
+                void print(tree * t, std::string offset) {
+                    std::cout << offset << "Kind(" << to_string(type) << ")" << std::endl;
+                    if (op2 != npos) {
+                        t->get_node(op2)->print(t, offset + "    ");
+                    }
+                    if (op1 != npos) {
+                        t->get_node(op1)->print(t, offset + "    ");
+                    }
+                }
+            };
+
+            uint32_t new_node(kind k) {
+                nodes.push_back(node{.type = k});
+                return nodes.size() - 1;
+            }
+
+            void set_left(uint32_t par_idx, uint32_t ch_idx) {
+                auto parent = get_node(par_idx);
+                auto child = get_node(ch_idx);
+                parent->op1 = ch_idx;
+                child->par = par_idx;
+            }
+
+            void set_right(uint32_t par_idx, uint32_t ch_idx) {
+                auto parent = get_node(par_idx);
+                auto child = get_node(ch_idx);
+                parent->op2 = ch_idx;
+                child->par = par_idx;
+            }
+
+            node * left_child(node const * node) {
+                return &nodes[node->op1];
+            }
+
+            node * right_child(node const * node) {
+                return &nodes[node->op2];
+            }
+
+            node * parent(node const * node) {
+                return &nodes[node->par];
+            }
+
+            node * get_node(uint32_t idx) {
+                return &nodes[idx];
+            }
+
+            bool have_right(uint32_t idx) {
+                auto nd = get_node(idx);
+                return nd->op2 != node::npos;
+            }
+
+            bool is_scope_unfinished(uint32_t idx) {
+                return is_scope(get_node(idx)->type) && !have_right(idx);
+            }
+
+            std::vector<node> nodes;
+
+            void print() {
+                get_node(root)->print(this, "");
+            }
+        };
+
     }
-};
+
+    uint32_t parse_expression(ast::tree & tree, std::vector<lexer::token>::const_iterator & it) {
+        auto & tok = *(it++);
+        switch (tok.type) {
+            case lexer::kind::IDENTIFIER:
+                return tree.new_node(ast::kind::VAR);
+            case lexer::kind::CONSTANT:
+                return tree.new_node(ast::kind::CONST);
+            default:
+                break;
+        }
+        return ast::tree::node::npos;
+    }
+
+    ast::tree parse(std::vector<lexer::token> const & tokens) {
+        ast::tree result;
+
+        std::vector<uint32_t> pending;
+
+        for (auto it = tokens.begin(); it != tokens.end(); ) {
+            switch (it->type) {
+                case lexer::kind::WHITESPACE: {
+                    ++it;
+                    continue;
+                }
+
+                case lexer::kind::IF: {
+                    auto idx = result.new_node(ast::kind::IF);
+                    pending.push_back(idx);
+                    result.set_left(idx, parse_expression(result, it)); // todo
+                    break;
+                }
+
+                case lexer::kind::WHILE: {
+                    auto idx = result.new_node(ast::kind::WHILE);
+                    pending.push_back(idx);
+                    result.set_left(idx, parse_expression(result, it)); // todo
+                    break;
+                }
+
+                case lexer::kind::END: {
+                    auto statements = pending.back();
+                    pending.pop_back();
+
+                    while (!result.is_scope_unfinished(pending.back())) {
+                        auto seq = result.new_node(ast::kind::STATEMENTS);
+                        result.set_left(seq, pending.back());
+                        result.set_right(seq, statements);
+                        statements = seq;
+                        pending.pop_back();
+                    }
+
+                    result.set_right(pending.back(), statements);
+                    ++it;
+                    break;
+                }
+
+                default: {
+                    ++it;
+
+                    auto node = result.new_node(ast::kind::ASSIGNMENT);
+                    if (it->type == lexer::kind::WHITESPACE) ++it;
+                    assert(it->type == lexer::kind::ASSIGNMENT);
+                    ++it;
+                    if (it->type == lexer::kind::WHITESPACE) ++it;
+
+                    auto expr = parse_expression(result, it);
+                    result.set_left(node, result.new_node(ast::kind::VAR));
+                    result.set_right(node, expr);
+
+                    pending.push_back(node);
+                    break;
+                }
+            }
+        }
+
+        result.root = pending.back();
+
+        return result;
+    }
+
+}
